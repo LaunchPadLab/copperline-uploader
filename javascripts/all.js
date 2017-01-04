10dc4cec7df91c152d05c00e37fade6df5e47ff8
Monique Marchwiany <monique@launchpadlab.com>
2017-01-04 12:15:06 -0600

    Site updated to da11ed2

---
+++ javascripts/all.js
@@ -0,0 +1,342 @@
+jQuery(document).ready(function(event){
+  var isAnimating = false,
+    newLocation = '';
+    firstLoad = false;
+
+  //trigger smooth transition from the actual page to the new one
+  $('main').on('click', '[data-type="page-transition"]', function(event){
+    event.preventDefault();
+    //detect which page has been selected
+    var newPage = $(this).attr('href');
+    //if the page is not already being animated - trigger animation
+    if( !isAnimating ) changePage(newPage, true);
+    firstLoad = true;
+  });
+
+  //detect the 'popstate' event - e.g. user clicking the back button
+  $(window).on('popstate', function() {
+    if( firstLoad ) {
+      /*
+      Safari emits a popstate event on page load - check if firstLoad is true before animating
+      if it's false - the page has just been loaded
+      */
+      var newPageArray = location.pathname.split('/'),
+        //this is the url of the page to be loaded
+        newPage = newPageArray[newPageArray.length - 1];
+
+      if( !isAnimating  &&  newLocation != newPage ) changePage(newPage, false);
+    }
+    firstLoad = true;
+  });
+
+  function changePage(url, bool) {
+    isAnimating = true;
+    // trigger page animation
+    $('body').addClass('page-is-changing');
+    $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+      loadNewContent(url, bool);
+      newLocation = url;
+      $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    });
+    //if browser doesn't support CSS transitions
+    if( !transitionsSupported() ) {
+      loadNewContent(url, bool);
+      newLocation = url;
+    }
+  }
+
+  function loadNewContent(url, bool) {
+    url = ('' == url) ? 'index.html' : url;
+    var newSection = 'cd-'+url.replace('.html', '');
+    var section = $('<div class="cd-main-content '+newSection+'"></div>');
+
+    section.load(url+' .cd-main-content > *', function(event){
+      // load new content and replace <main> content with the new one
+      $('main').html(section);
+      //if browser doesn't support CSS transitions - dont wait for the end of transitions
+      var delay = ( transitionsSupported() ) ? 1200 : 0;
+      setTimeout(function(){
+        //wait for the end of the transition on the loading bar before revealing the new content
+        ( section.hasClass('cd-about') ) ? $('body').addClass('cd-about') : $('body').removeClass('cd-about');
+        $('body').removeClass('page-is-changing');
+        $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+          isAnimating = false;
+          $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+        });
+
+        if( !transitionsSupported() ) isAnimating = false;
+      }, delay);
+
+      if(url!=window.location && bool){
+        //add the new page to the window.history
+        //if the new page was triggered by a 'popstate' event, don't add it
+        window.history.pushState({path: url},'',url);
+      }
+    });
+  }
+
+  function transitionsSupported() {
+    return $('html').hasClass('csstransitions');
+  }
+});
+$(document).ready(function() {
+  //jQuery time
+  var current_fs, next_fs, previous_fs; //fieldsets
+  var left, opacity, scale; //fieldset properties which we will animate
+  var animating; //flag to prevent quick multi-click glitches
+
+  $(".next").click(function(){
+    if(animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //activate next step on progressbar using the index of next_fs
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+      step: function(now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale current_fs down to 80%
+        scale = 1 - (1 - now) * 0.2;
+        //2. bring next_fs from the right(50%)
+        left = (now * 50)+"%";
+        //3. increase opacity of next_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({'transform': 'scale('+scale+')'});
+        next_fs.css({'left': left, 'opacity': opacity});
+      },
+      duration: 800,
+      complete: function(){
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  });
+
+  $(".previous").click(function(){
+    if(animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //de-activate current step on progressbar
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+      step: function(now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale previous_fs from 80% to 100%
+        scale = 0.8 + (1 - now) * 0.2;
+        //2. take current_fs to the right(50%) - from 0%
+        left = ((1-now) * 50)+"%";
+        //3. increase opacity of previous_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({'left': left});
+        previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+      },
+      duration: 800,
+      complete: function(){
+        current_fs.hide();
+        animating = false;
+      },
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  });
+
+  $(".submit").click(function(){
+    return false;
+  })
+});
+
+$("document").ready(function() {
+  $(".slider").rangeslider();
+});
+$.fn.rangeslider = function(options) {
+  var obj = this;
+  var defautValue = obj.attr("value");
+  obj.wrap("<span class='range-slider'></span>");
+  obj.after("<span class='slider-container'><span class='bar'><span></span></span><span class='bar-btn'><span class='bar-value'>$0</span></span></span>");
+  obj.attr("oninput", "updateSlider(this)");
+  updateSlider(this);
+  return obj;
+};
+
+function updateSlider(passObj) {
+  var obj = $(passObj);
+  var value = obj.val();
+  var min = obj.attr("min");
+  var max = obj.attr("max");
+  var range = Math.round(max - min);
+  var percentage = Math.round((value - min) * 100 / range);
+  var nextObj = obj.next();
+  nextObj.find("span.bar-btn").css("left", percentage + "%");
+  nextObj.find("span.bar > span").css("width", percentage + "%");
+  nextObj.find("span.bar-btn > span").text(value);
+};
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
+/*!
+ * animsition v4.0.2
+ * A simple and easy jQuery plugin for CSS animated page transitions.
+ * http://blivesta.github.io/animsition
+ * License : MIT
+ * Author : blivesta (http://blivesta.com/)
+ */
+
+!function(t){"use strict";"function"==typeof define&&define.amd?define(["jquery"],t):"object"==typeof exports?module.exports=t(require("jquery")):t(jQuery)}(function(t){"use strict";var n="animsition",i={init:function(a){a=t.extend({inClass:"fade-in",outClass:"fade-out",inDuration:1500,outDuration:800,linkElement:".animsition-link",loading:!0,loadingParentElement:"body",loadingClass:"animsition-loading",loadingInner:"",timeout:!1,timeoutCountdown:5e3,onLoadEvent:!0,browser:["animation-duration","-webkit-animation-duration"],overlay:!1,overlayClass:"animsition-overlay-slide",overlayParentElement:"body",transition:function(t){window.location.href=t}},a),i.settings={timer:!1,data:{inClass:"animsition-in-class",inDuration:"animsition-in-duration",outClass:"animsition-out-class",outDuration:"animsition-out-duration",overlay:"animsition-overlay"},events:{inStart:"animsition.inStart",inEnd:"animsition.inEnd",outStart:"animsition.outStart",outEnd:"animsition.outEnd"}};var o=i.supportCheck.call(this,a);if(!o&&a.browser.length>0&&(!o||!this.length))return"console"in window||(window.console={},window.console.log=function(t){return t}),this.length||console.log("Animsition: Element does not exist on page."),o||console.log("Animsition: Does not support this browser."),i.destroy.call(this);var e=i.optionCheck.call(this,a);return e&&t("."+a.overlayClass).length<=0&&i.addOverlay.call(this,a),a.loading&&t("."+a.loadingClass).length<=0&&i.addLoading.call(this,a),this.each(function(){var o=this,e=t(this),s=t(window),r=t(document),l=e.data(n);l||(a=t.extend({},a),e.data(n,{options:a}),a.timeout&&i.addTimer.call(o),a.onLoadEvent&&s.on("load."+n,function(){i.settings.timer&&clearTimeout(i.settings.timer),i["in"].call(o)}),s.on("pageshow."+n,function(t){t.originalEvent.persisted&&i["in"].call(o)}),s.on("unload."+n,function(){}),r.on("click."+n,a.linkElement,function(n){n.preventDefault();var a=t(this),e=a.attr("href");2===n.which||n.metaKey||n.shiftKey||-1!==navigator.platform.toUpperCase().indexOf("WIN")&&n.ctrlKey?window.open(e,"_blank"):i.out.call(o,a,e)}))})},addOverlay:function(n){t(n.overlayParentElement).prepend('<div class="'+n.overlayClass+'"></div>')},addLoading:function(n){t(n.loadingParentElement).append('<div class="'+n.loadingClass+'">'+n.loadingInner+"</div>")},removeLoading:function(){var i=t(this),a=i.data(n).options,o=t(a.loadingParentElement).children("."+a.loadingClass);o.fadeOut().remove()},addTimer:function(){var a=this,o=t(this),e=o.data(n).options;i.settings.timer=setTimeout(function(){i["in"].call(a),t(window).off("load."+n)},e.timeoutCountdown)},supportCheck:function(n){var i=t(this),a=n.browser,o=a.length,e=!1;0===o&&(e=!0);for(var s=0;o>s;s++)if("string"==typeof i.css(a[s])){e=!0;break}return e},optionCheck:function(n){var a,o=t(this);return a=n.overlay||o.data(i.settings.data.overlay)?!0:!1},animationCheck:function(i,a,o){var e=t(this),s=e.data(n).options,r=typeof i,l=!a&&"number"===r,d=a&&"string"===r&&i.length>0;return l||d?i=i:a&&o?i=s.inClass:!a&&o?i=s.inDuration:a&&!o?i=s.outClass:a||o||(i=s.outDuration),i},"in":function(){var a=this,o=t(this),e=o.data(n).options,s=o.data(i.settings.data.inDuration),r=o.data(i.settings.data.inClass),l=i.animationCheck.call(a,s,!1,!0),d=i.animationCheck.call(a,r,!0,!0),u=i.optionCheck.call(a,e),c=o.data(n).outClass;e.loading&&i.removeLoading.call(a),c&&o.removeClass(c),u?i.inOverlay.call(a,d,l):i.inDefault.call(a,d,l)},inDefault:function(n,a){var o=t(this);o.css({"animation-duration":a+"ms"}).addClass(n).trigger(i.settings.events.inStart).animateCallback(function(){o.removeClass(n).css({opacity:1}).trigger(i.settings.events.inEnd)})},inOverlay:function(a,o){var e=t(this),s=e.data(n).options;e.css({opacity:1}).trigger(i.settings.events.inStart),t(s.overlayParentElement).children("."+s.overlayClass).css({"animation-duration":o+"ms"}).addClass(a).animateCallback(function(){e.trigger(i.settings.events.inEnd)})},out:function(a,o){var e=this,s=t(this),r=s.data(n).options,l=a.data(i.settings.data.outClass),d=s.data(i.settings.data.outClass),u=a.data(i.settings.data.outDuration),c=s.data(i.settings.data.outDuration),m=l?l:d,g=u?u:c,f=i.animationCheck.call(e,m,!0,!1),v=i.animationCheck.call(e,g,!1,!1),h=i.optionCheck.call(e,r);s.data(n).outClass=f,h?i.outOverlay.call(e,f,v,o):i.outDefault.call(e,f,v,o)},outDefault:function(a,o,e){var s=t(this),r=s.data(n).options;s.css({"animation-duration":o+1+"ms"}).addClass(a).trigger(i.settings.events.outStart).animateCallback(function(){s.trigger(i.settings.events.outEnd),r.transition(e)})},outOverlay:function(a,o,e){var s=this,r=t(this),l=r.data(n).options,d=r.data(i.settings.data.inClass),u=i.animationCheck.call(s,d,!0,!0);t(l.overlayParentElement).children("."+l.overlayClass).css({"animation-duration":o+1+"ms"}).removeClass(u).addClass(a).trigger(i.settings.events.outStart).animateCallback(function(){r.trigger(i.settings.events.outEnd),l.transition(e)})},destroy:function(){return this.each(function(){var i=t(this);t(window).off("."+n),i.css({opacity:1}).removeData(n)})}};t.fn.animateCallback=function(n){var i="animationend webkitAnimationEnd";return this.each(function(){var a=t(this);a.on(i,function(){return a.off(i),n.call(this)})})},t.fn.animsition=function(a){return i[a]?i[a].apply(this,Array.prototype.slice.call(arguments,1)):"object"!=typeof a&&a?void t.error("Method "+a+" does not exist on jQuery."+n):i.init.apply(this,arguments)}});
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'flip-in-x-fr',
+    outClass: 'flip-out-x-fr',
+    inDuration: 1500,
+    outDuration: 1500,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+});
+//jQuery plugin
+(function( $ ) {
+
+   $.fn.uploader = function( options ) {
+     var settings = $.extend({
+       MessageAreaText: "No files selected.",
+       DefaultErrorMessage: "Unable to open this file.",
+       BadTypeErrorMessage: "We cannot accept this file type at this time.",
+       acceptedFileTypes: ['pdf', 'jpg', 'gif', 'jpeg', 'bmp', 'tif', 'tiff', 'png', 'xps', 'doc', 'docx',
+        'fax', 'wmp', 'ico', 'txt', 'cs', 'rtf', 'xls', 'xlsx']
+     }, options );
+
+     var uploadId = 1;
+     //update the messaging
+      $('.file-uploader__message-area p').text(options.MessageAreaText || settings.MessageAreaText);
+
+     //create and add the file list and the hidden input list
+     var fileList = $('<ul class="file-list"></ul>');
+     var hiddenInputs = $('<div class="hidden-inputs hidden"></div>');
+     $('.file-uploader__message-area').after(fileList);
+     $('.file-list').after(hiddenInputs);
+
+    //when choosing a file, add the name to the list and copy the file input into the hidden inputs
+     $('.file-chooser__input').on('change', function(){
+        var file = $('.file-chooser__input').val();
+        var fileName = (file.match(/([^\\\/]+)$/)[0]);
+
+       //clear any error condition
+       $('.file-chooser').removeClass('error');
+       $('.error-message').remove();
+
+       //validate the file
+       var check = checkFile(fileName);
+       if(check === "valid") {
+
+         // move the 'real' one to hidden list
+         $('.hidden-inputs').append($('.file-chooser__input'));
+
+         //insert a clone after the hiddens (copy the event handlers too)
+         $('.file-chooser').append($('.file-chooser__input').clone({ withDataAndEvents: true}));
+
+         //add the name and a remove button to the file-list
+         $('.file-list').append('<li style="display: none;"><span class="file-list__name">' + fileName + '</span><button class="removal-button" data-uploadid="'+ uploadId +'"></button></li>');
+         $('.file-list').find("li:last").show(800);
+
+         //removal button handler
+         $('.removal-button').on('click', function(e){
+           e.preventDefault();
+
+           //remove the corresponding hidden input
+           $('.hidden-inputs input[data-uploadid="'+ $(this).data('uploadid') +'"]').remove();
+
+           //remove the name from file-list that corresponds to the button clicked
+           $(this).parent().hide("puff").delay(10).queue(function(){$(this).remove();});
+
+           //if the list is now empty, change the text back
+           if($('.file-list li').length === 0) {
+             $('.file-uploader__message-area').text(options.MessageAreaText || settings.MessageAreaText);
+           }
+         });
+
+         //so the event handler works on the new "real" one
+         $('.hidden-inputs .file-chooser__input').removeClass('file-chooser__input').attr('data-uploadId', uploadId);
+
+         //update the message area
+         $('.file-uploader__message-area').text(options.MessageAreaTextWithFiles || settings.MessageAreaTextWithFiles);
+
+         uploadId++;
+
+       } else {
+         //indicate that the file is not ok
+         $('.file-chooser').addClass("error");
+         var errorText = options.DefaultErrorMessage || settings.DefaultErrorMessage;
+
+         if(check === "badFileName") {
+           errorText = options.BadTypeErrorMessage || settings.BadTypeErrorMessage;
+         }
+
+         $('.file-chooser__input').after('<p class="error-message">'+ errorText +'</p>');
+       }
+     });
+
+     var checkFile = function(fileName) {
+       var accepted          = "invalid",
+           acceptedFileTypes = this.acceptedFileTypes || settings.acceptedFileTypes,
+           regex;
+
+       for ( var i = 0; i < acceptedFileTypes.length; i++ ) {
+         regex = new RegExp("\\." + acceptedFileTypes[i] + "$", "i");
+
+         if ( regex.test(fileName) ) {
+           accepted = "valid";
+           break;
+         } else {
+           accepted = "badFileName";
+         }
+       }
+
+       return accepted;
+    };
+  };
+}( jQuery ));
+
+//init
+$(document).ready(function(){
+  $('.fileUploader').uploader({
+    MessageAreaText: "Upload 3 banks statements"
+  });
+});
+
+
+
+
+
+
+
