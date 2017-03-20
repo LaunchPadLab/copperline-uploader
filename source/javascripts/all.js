40cd8405de5ed7d18500d10c5e38facc8667605b
Monique Marchwiany <monique@launchpadlab.com>
2017-03-20 11:05:38 -0500

    added masking

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,3 +5,4 @@
 //= require animsition
 //= require animation
 //= require uploader
+//= require masking
---
+++ source/javascripts/masking.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('#acctNum').mask('0000 0000 0000 0000');
+});
